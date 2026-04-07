abd577a62f34a9c05a5f6beb9267e66fe8cb15dc
DESKTOP-TG6HNLE\Act-PC-11_yuri <kyl1563@naver.com>
2026-04-07 15:44:57 +0900

    2026.04.07 - 김유리 _발주 품목 조회 수정

--- src/main/java/mes/app/balju/BaljuOrderController.java
+++ src/main/java/mes/app/balju/BaljuOrderController.java
@@ -77,8 +77,8 @@ public class BaljuOrderController {
   @PostMapping("/multi_save")
   @Transactional
   public AjaxResult saveBaljuMulti(@RequestBody Map<String, Object> payload, Authentication auth) {
-    log.info("💾 [발주등록 시작] payload keys: {}", payload.keySet());
-    log.info("🧾 items 내용: {}", payload.get("items"));
+//    log.info("💾 [발주등록 시작] payload keys: {}", payload.keySet());
+//    log.info("🧾 items 내용: {}", payload.get("items"));
 
     User user = (User) auth.getPrincipal();
 
@@ -1033,15 +1033,11 @@ public class BaljuOrderController {
       data.put("user_id", user.getId());
       data.put("BALJUDATE", baljuDate);
 
-      int saveCount = this.baljuOrderService.SaveUnitPrice(data);
+      this.baljuOrderService.SaveUnitPrice(data);
 
-      if (saveCount > 0) {
-        result.success = true;
-      } else {
-        result.success = false;
-        result.message = "저장 실패: 중복된 데이터이거나 입력값이 올바르지 않습니다.";
-      }
-    }catch (Exception e) {
+      result.success = true;
+
+    } catch (Exception e) {
       result.success = false;
       result.message = "서버 오류: " + e.getMessage();
     }

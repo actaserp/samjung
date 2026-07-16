b2586dc39db8705ae2e4257ca2f7074356201a30
DESKTOP-TG6HNLE\Act-PC-11_yuri <kyl1563@naver.com>
2026-07-16 10:14:11 +0900

    2026.07.16 - 김유리 : 발주현황 데이터 중복 오류 수정

--- src/main/java/mes/app/balju/service/BalJuListServicr.java
+++ src/main/java/mes/app/balju/service/BalJuListServicr.java
@@ -27,61 +27,65 @@ public class BalJuListServicr {
     paramMap.addValue("end", end);
     paramMap.addValue("spjangcd", spjangcd);
 
-    String sql= """     
-        SELECT 
-          h.BALJUNUM,
-          STUFF(STUFF(h.BALJUDATE, 5, 0, '-'), 8, 0, '-') AS BALJUDATE,
-          d.BALJUSEQ,
-          d.pmapseq,
-          d.pname,
-          d.PCODE,
-          c2.unit as punit,
-		  c2.spec as pspec,
-          c2.PART_SIZE as psize,
-          d.pqty,
-          d.puamt,
-          d.pamt,
-          d.pmapseq,
-          CASE d.CHULFLAG
-            WHEN '1' THEN '출고'
-            ELSE '미출고'
-          END AS CHULFLAG,
-          STUFF(STUFF(d.CHULDATE, 5, 0, '-'), 8, 0, '-') AS CHULDATE,
-          CASE d.FACFLAG
-            WHEN '1' THEN '출고'
-            ELSE '미출고'
-          END AS facflag,
-          STUFF(STUFF(d.FACDATE, 5, 0, '-'), 8, 0, '-') AS FACDATE,
-          CASE d.HYUNFLAG
-            WHEN '1' THEN '확인'
-            ELSE '미확인'
-          END AS hyunflag,
-          STUFF(STUFF(d.HYUNDATE, 5, 0, '-'), 8, 0, '-') AS HYUNDATE,
-          d.remark,
-          h.CLTCD,
-          c.CLTNM,
-          h.PROCD as project_no,
-          STUFF(STUFF(h.ICHDATE, 5, 0, '-'), 8, 0, '-') AS ichdate,
-          h.PERNM,
-          h.ACTNM,
-          au3.last_name AS CHULPERNM,
-          au2.last_name AS FACPERNM,
-          au1.last_name AS HYUNPERNM,
-             total_amt.TOTAL_PAMT
-        FROM TB_CA661 d
-        JOIN TB_CA660 h ON d.BALJUNUM = h.BALJUNUM
-        left join tb_ca662 c2 on d.PCODE = c2.PART_NO
-        LEFT JOIN (
-          SELECT BALJUNUM, SUM(PAMT) AS TOTAL_PAMT
-          FROM TB_CA661
-          GROUP BY BALJUNUM
-        ) total_amt ON d.BALJUNUM = total_amt.BALJUNUM
-        LEFT JOIN TB_XCLIENT c ON c.CLTCD = h.CLTCD
-        LEFT JOIN auth_user au1 ON d.HYUNPERNM = au1.username
-        LEFT JOIN auth_user au2 ON d.FACPERNM = au2.username
-        LEFT JOIN auth_user au3 ON d.CHULPERNM = au3.username
-        WHERE h.SPJANGCD = :spjangcd
-        """;
+    String sql = """
+      SELECT
+        h.BALJUNUM,
+        STUFF(STUFF(h.BALJUDATE, 5, 0, '-'), 8, 0, '-') AS BALJUDATE,
+        d.BALJUSEQ,
+        d.pmapseq,
+        d.pname,
+        d.PCODE,
+        c2.unit as punit,
+        c2.spec as pspec,
+        c2.PART_SIZE as psize,
+        d.pqty,
+        d.puamt,
+        d.pamt,
+        CASE d.CHULFLAG
+          WHEN '1' THEN '출고'
+          ELSE '미출고'
+        END AS CHULFLAG,
+        STUFF(STUFF(d.CHULDATE, 5, 0, '-'), 8, 0, '-') AS CHULDATE,
+        CASE d.FACFLAG
+          WHEN '1' THEN '출고'
+          ELSE '미출고'
+        END AS facflag,
+        STUFF(STUFF(d.FACDATE, 5, 0, '-'), 8, 0, '-') AS FACDATE,
+        CASE d.HYUNFLAG
+          WHEN '1' THEN '확인'
+          ELSE '미확인'
+        END AS hyunflag,
+        STUFF(STUFF(d.HYUNDATE, 5, 0, '-'), 8, 0, '-') AS HYUNDATE,
+        d.remark,
+        h.CLTCD,
+        c.CLTNM,
+        h.PROCD as project_no,
+        STUFF(STUFF(h.ICHDATE, 5, 0, '-'), 8, 0, '-') AS ichdate,
+        h.PERNM,
+        h.ACTNM,
+        au3.last_name AS CHULPERNM,
+        au2.last_name AS FACPERNM,
+        au1.last_name AS HYUNPERNM,
+        total_amt.TOTAL_PAMT
+      FROM TB_CA661 d
+      JOIN TB_CA660 h ON d.BALJUNUM = h.BALJUNUM
+      OUTER APPLY (
+        SELECT TOP 1 x.UNIT, x.SPEC, x.PART_SIZE
+        FROM tb_ca662 x
+        WHERE x.PART_NO = d.PCODE
+        ORDER BY x.BPDATE DESC, x.BPID DESC
+      ) c2
+      LEFT JOIN (
+        SELECT BALJUNUM, SUM(PAMT) AS TOTAL_PAMT
+        FROM TB_CA661
+        GROUP BY BALJUNUM
+      ) total_amt ON d.BALJUNUM = total_amt.BALJUNUM
+      LEFT JOIN TB_XCLIENT c ON c.CLTCD = h.CLTCD
+      LEFT JOIN auth_user au1 ON d.HYUNPERNM = au1.username
+      LEFT JOIN auth_user au2 ON d.FACPERNM = au2.username
+      LEFT JOIN auth_user au3 ON d.CHULPERNM = au3.username
+      WHERE h.SPJANGCD = :spjangcd
+      """;
 
     // 날짜 조건
     if ("sales".equalsIgnoreCase(date_kind)) {
@@ -99,15 +103,16 @@ public class BalJuListServicr {
       paramMap.addValue("cltnm", "%" + cltnm + "%");
     }
 
-   sql+= """
-        ORDER BY h.BALJUNUM, d.BALJUSEQ 
-       """;
+    sql += """
+       ORDER BY h.BALJUNUM, d.BALJUSEQ
+      """;
 //    log.info("발주 list read SQL: {}", sql);
 //    log.info("SQL Parameters: {}", paramMap.getValues());
     return sqlRunner.getRows(sql, paramMap);
   }
 
-  public List<Map<String, Object>> getBalJuItemList(String pname, String actnm, String date_kind, Timestamp start, Timestamp end, String spjangcd) {
+  public List<Map<String, Object>> getBalJuItemList(String pname, String actnm, String date_kind, Timestamp start,
+                                                    Timestamp end, String spjangcd) {
     MapSqlParameterSource paramMap = new MapSqlParameterSource();
     paramMap.addValue("pname", pname);
     paramMap.addValue("actnm", actnm);
@@ -116,25 +121,30 @@ public class BalJuListServicr {
     paramMap.addValue("end", end);
     paramMap.addValue("spjangcd", spjangcd);
 
-    String sql= """
-        SELECT
-          d.pname,
-          c2.unit as punit,
-		  c2.spec as pspec,
-          c2.PART_SIZE as psize,
-          c.cltnm,
-          STUFF(STUFF( h.baljudate, 5, 0, '-'), 8, 0, '-') AS baljudate,
-          d.pqty,
-          d.puamt,
-          d.pamt,
-          STUFF(STUFF(h.ICHDATE, 5, 0, '-'), 8, 0, '-') AS ichdate,
-          h.actnm
-        FROM TB_CA661 d
-        JOIN TB_CA660 h ON d.BALJUNUM = h.BALJUNUM
-        LEFT JOIN TB_XCLIENT c ON c.cltcd = h.cltcd
-        left join tb_ca662 c2 on d.PCODE = c2.PART_NO
-         where 1=1
-        """;
+    String sql = """
+      SELECT
+        d.pname,
+        c2.unit as punit,
+        c2.spec as pspec,
+        c2.PART_SIZE as psize,
+        c.cltnm,
+        STUFF(STUFF(h.baljudate, 5, 0, '-'), 8, 0, '-') AS baljudate,
+        d.pqty,
+        d.puamt,
+        d.pamt,
+        STUFF(STUFF(h.ICHDATE, 5, 0, '-'), 8, 0, '-') AS ichdate,
+        h.actnm
+      FROM TB_CA661 d
+      JOIN TB_CA660 h ON d.BALJUNUM = h.BALJUNUM
+      LEFT JOIN TB_XCLIENT c ON c.cltcd = h.cltcd
+      OUTER APPLY (
+        SELECT TOP 1 x.UNIT, x.SPEC, x.PART_SIZE
+        FROM tb_ca662 x
+        WHERE x.PART_NO = d.PCODE
+        ORDER BY x.BPDATE DESC, x.BPID DESC
+      ) c2
+      WHERE h.SPJANGCD = :spjangcd
+      """;
 
     if ("sales".equalsIgnoreCase(date_kind)) {
       sql += " AND h.BALJUDATE BETWEEN :start AND :end ";
@@ -152,78 +162,12 @@ public class BalJuListServicr {
       paramMap.addValue("actnm", "%" + actnm + "%");
     }
 
-    sql+= """
-        ORDER BY h.baljudate DESC , d.pname ASC;
-        """;
-
+    sql += """
+       ORDER BY h.baljudate DESC, d.pname ASC
+      """;
 //    log.info("발주 item list read SQL: {}", sql);
 //    log.info("SQL Parameters: {}", paramMap.getValues());
     return sqlRunner.getRows(sql, paramMap);
-
   }
-
-  /*public List<Map<String, Object>> getBalJuItemList(String pname, String actnm, String date_kind, Timestamp start, Timestamp end, String spjangcd) {
-    MapSqlParameterSource paramMap = new MapSqlParameterSource();
-    paramMap.addValue("start", start);
-    paramMap.addValue("end", end);
-    paramMap.addValue("spjangcd", spjangcd);
-
-    String sql = """
-        WITH ItemSummary AS (
-          SELECT
-            d.pname,
-            d.psize,
-            d.punit,
-            h.baljudate,
-            SUM(d.pqty) AS total_pqty,
-            AVG(d.puamt) AS avg_puamt,
-            SUM(d.pamt) AS total_pamt
-          FROM TB_CA661 d
-          JOIN TB_CA660 h ON d.BALJUNUM = h.BALJUNUM
-          WHERE 1=1 """;
-
-    if ("sales".equalsIgnoreCase(date_kind)) {
-      sql += " AND h.BALJUDATE BETWEEN :start AND :end ";
-    } else {
-      sql += " AND h.ICHDATE BETWEEN :start AND :end ";
-    }
-    if (pname != null && !pname.isEmpty()) {
-      sql += " AND d.pname LIKE :pname \n";
-      paramMap.addValue("pname", "%" + pname + "%");
-    }
-    sql += """
-          GROUP BY d.pname, d.psize, d.punit, h.baljudate
-        )
-        SELECT
-          s.pname,
-          s.psize,
-          s.punit,
-          STUFF(STUFF(s.baljudate, 5, 0, '-'), 8, 0, '-') AS baljudate,
-          s.total_pqty,
-          s.avg_puamt,
-          s.total_pamt,
-          MAX(c.cltnm) AS cltnm,
-          MAX(STUFF(STUFF(h.ICHDATE, 5, 0, '-'), 8, 0, '-')) AS ichdate,
-          MAX(h.actnm) AS actnm
-        FROM ItemSummary s
-        JOIN TB_CA661 d ON d.pname = s.pname AND d.psize = s.psize AND d.punit = s.punit
-        JOIN TB_CA660 h ON h.baljunum = d.baljunum AND h.baljudate = s.baljudate
-        LEFT JOIN TB_XCLIENT c ON c.cltcd = h.cltcd
-        WHERE 1=1
-        """;
-
-    if (actnm != null && !actnm.isEmpty()) {
-      sql += " AND h.actnm LIKE :actnm \n";
-      paramMap.addValue("actnm", "%" + actnm + "%");
-    }
-
-    sql += """
-        GROUP BY s.pname, s.psize, s.punit, s.baljudate, s.total_pqty, s.avg_puamt, s.total_pamt
-        ORDER BY s.baljudate DESC, s.pname ASC
-        """;
-    log.info("발주 item list read SQL: {}", sql);
-    log.info("SQL Parameters: {}", paramMap.getValues());
-    return sqlRunner.getRows(sql, paramMap);
-  }*/
 
 }
